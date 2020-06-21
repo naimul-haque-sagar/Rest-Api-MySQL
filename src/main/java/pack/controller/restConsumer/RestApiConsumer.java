d088f034c9d4ebf6d92fbc4a199e1a5ca1779127
naimul <naimulhaquesagar24@gmail.com>
2020-06-21 20:14:36 +0600

    rest link added

--- src/main/java/pack/controller/restConsumer/RestApiConsumer.java
+++ src/main/java/pack/controller/restConsumer/RestApiConsumer.java
@@ -19,89 +19,84 @@ import java.util.List;
 public class RestApiConsumer {
     private final RestTemplate restTemplate;
 
+    String POST_MOVIE_DETAILS="http://localhost:8080/api/movieDetails/postMovieDetails";
+    String GET_MOVIE_DETAILS="http://localhost:8080/api/movieDetails/getMovieDetails";
+    String GET_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/getMovieDetailsById";
+    String GET_MOVIE_DETAILS_BY_NAME="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
+    String UPDATE_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/updateMovieDetailsById";
+    String DELETE_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/deleteMovieDetailsById";
+
     @PostMapping("postMovieDetails1")
     public ResponseEntity postMovieDetails1(@RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/postMovieDetails";
-        restTemplate.postForObject(url,movieDetailsDto,MovieDetailsDto.class);
+        restTemplate.postForObject(POST_MOVIE_DETAILS,movieDetailsDto,MovieDetailsDto.class);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("postMovieDetails2")
     public ResponseEntity postMovieDetails2(@RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/postMovieDetails";
-        return restTemplate.postForEntity(url,movieDetailsDto,MovieDetailsDto.class);
+        return restTemplate.postForEntity(POST_MOVIE_DETAILS,movieDetailsDto,MovieDetailsDto.class);
     }
 
     @GetMapping("getMovieDetails1")
     public ResponseEntity<List<Object>> getAllMovieDetails1(){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetails";
-        Object[] objects=restTemplate.getForObject(url,Object[].class);
+        Object[] objects=restTemplate.getForObject(GET_MOVIE_DETAILS,Object[].class);
         return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(objects));
     }
 
     @GetMapping("getMovieDetails2")
     public ResponseEntity<List<MovieDetailsDto>> getAllMovieDetails2(){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetails";
-        MovieDetailsDto[] objects=restTemplate.getForObject(url,MovieDetailsDto[].class);
+        MovieDetailsDto[] objects=restTemplate.getForObject(GET_MOVIE_DETAILS,MovieDetailsDto[].class);
         return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(objects));
     }
 
     @GetMapping("getMovieDetails3")
     public ResponseEntity<List<MovieDetailsDto>> getAllMovieDetails3(){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetails";
-        ResponseEntity<MovieDetailsDto[]> movieDetails=restTemplate.getForEntity(url,MovieDetailsDto[].class);
+        ResponseEntity<MovieDetailsDto[]> movieDetails=restTemplate.getForEntity(GET_MOVIE_DETAILS,MovieDetailsDto[].class);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(movieDetails.getBody() != null? Arrays.asList(movieDetails.getBody()):Collections.emptyList());
     }
 
     @GetMapping("getMovieDetailsById/{id}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsById(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsById";
         MovieDetailsDto movieDetailsDto=restTemplate
-                .getForObject(url+"/{id}",MovieDetailsDto.class,id);
+                .getForObject(GET_MOVIE_DETAILS_BY_ID+"/{id}",MovieDetailsDto.class,id);
         return ResponseEntity.status(HttpStatus.OK).body(movieDetailsDto);
     }
 
     @GetMapping("getMovieDetailsByid/{id}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsByid(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsById";
         ResponseEntity<MovieDetailsDto> movieDetailsDto=restTemplate
-                .getForEntity(url+"/{id}",MovieDetailsDto.class,id);
+                .getForEntity(GET_MOVIE_DETAILS_BY_ID+"/{id}",MovieDetailsDto.class,id);
         return movieDetailsDto;
     }
 
     @GetMapping("getMovieDetailsByName/{name}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsByName(@PathVariable String name){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
-        MovieDetailsDto movieDetailsDto=restTemplate.getForObject(url+"/{name}",MovieDetailsDto.class,name);
+        MovieDetailsDto movieDetailsDto=restTemplate.getForObject(GET_MOVIE_DETAILS_BY_NAME+"/{name}",MovieDetailsDto.class,name);
         return ResponseEntity.status(HttpStatus.OK).body(movieDetailsDto);
     }
 
     @GetMapping("getMovieDetailsByname/{name}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsByname(@PathVariable String name){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
-        return restTemplate.getForEntity(url+"/{name}",MovieDetailsDto.class,name);
+        return restTemplate.getForEntity(GET_MOVIE_DETAILS_BY_NAME+"/{name}",MovieDetailsDto.class,name);
     }
 
     @PutMapping("updateMovieDetailsById/{id}")
     public ResponseEntity updateMovieDetailsById(@PathVariable Long id, @RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/updateMovieDetailsById";
-        restTemplate.put(url+"/{id}",movieDetailsDto,id);
+        restTemplate.put(UPDATE_MOVIE_DETAILS_BY_ID+"/{id}",movieDetailsDto,id);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping("updateMovieDetailsByid/{id}")
     public ResponseEntity updateMovieDetailsByid(@PathVariable Long id, @RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/updateMovieDetailsById";
         return restTemplate
-                .exchange(url+"/{id}", HttpMethod.PUT,new HttpEntity<>(movieDetailsDto),
+                .exchange(UPDATE_MOVIE_DETAILS_BY_ID+"/{id}", HttpMethod.PUT,new HttpEntity<>(movieDetailsDto),
                         MovieDetailsDto.class,id);
     }
 
     @DeleteMapping("deleteMovieDetailsById/{id}")
     public ResponseEntity deleteMovieDetailsById(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/deleteMovieDetailsById";
-        restTemplate.delete(url+"/{id}",id);
+        restTemplate.delete(DELETE_MOVIE_DETAILS_BY_ID+"/{id}",id);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/pack/controller/restConsumer/RestApiConsumer2.java
+++ src/main/java/pack/controller/restConsumer/RestApiConsumer2.java
@@ -19,79 +19,75 @@ import java.util.List;
 public class RestApiConsumer2 {
     private final WebClient.Builder webClientBuilder;
 
+    String GET_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/getMovieDetailsById";
+    String GET_MOVIE_DETAILS_BY_NAME="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
+    String GET_MOVIE_DETAILS="http://localhost:8080/api/movieDetails/getMovieDetails";
+    String DELETE_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/deleteMovieDetailsById";
+    String UPDATE_MOVIE_DETAILS_BY_ID="http://localhost:8080/api/movieDetails/updateMovieDetailsById";
+    String POST_MOVIE_DETAILS="http://localhost:8080/api/movieDetails/postMovieDetails";
+
     @GetMapping("getMovieDetailsByid/{id}")
     public Mono<MovieDetailsDto> getMovieDetailsByid(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsById";
-        return webClientBuilder.build().get().uri(url+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class);
+        return webClientBuilder.build().get().uri(GET_MOVIE_DETAILS_BY_ID+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class);
     }
 
     @GetMapping("getMovieDetailsByname/{name}")
     public Mono<MovieDetailsDto> getMovieDetailsByname(@PathVariable String name){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
-        return webClientBuilder.build().get().uri(url+"/"+name).retrieve().bodyToMono(MovieDetailsDto.class);
+        return webClientBuilder.build().get().uri(GET_MOVIE_DETAILS_BY_NAME+"/"+name).retrieve().bodyToMono(MovieDetailsDto.class);
     }
 
     @GetMapping("getMovieDetails")
     public Flux<MovieDetailsDto> getAllMovieDetails(){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetails";
-        return webClientBuilder.build().get().uri(url).retrieve().bodyToFlux(MovieDetailsDto.class);
+        return webClientBuilder.build().get().uri(GET_MOVIE_DETAILS).retrieve().bodyToFlux(MovieDetailsDto.class);
     }
 
     @PostMapping("postMovieDetails2")
     public Mono<MovieDetailsDto> postMovieDetails2(@RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/postMovieDetails";
         return webClientBuilder.build()
-                .post().uri(url).body(BodyInserters.fromValue(movieDetailsDto)).retrieve().bodyToMono(MovieDetailsDto.class);
+                .post().uri(POST_MOVIE_DETAILS).body(BodyInserters.fromValue(movieDetailsDto)).retrieve().bodyToMono(MovieDetailsDto.class);
     }
 
     @PostMapping("postMovieDetails")
     public Mono<MovieDetailsDto> postMovieDetails(@RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/postMovieDetails";
         return webClientBuilder.build()
-                .post().uri(url).syncBody(movieDetailsDto).retrieve().bodyToMono(MovieDetailsDto.class);
+                .post().uri(POST_MOVIE_DETAILS).syncBody(movieDetailsDto).retrieve().bodyToMono(MovieDetailsDto.class);
     }
 
     @PostMapping("postMovieDetails1")
     public Mono<String> postMovieDetails1(@RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/postMovieDetails";
         return webClientBuilder.build()
-                .post().uri(url).syncBody(movieDetailsDto).retrieve().bodyToMono(String.class);
+                .post().uri(POST_MOVIE_DETAILS).syncBody(movieDetailsDto).retrieve().bodyToMono(String.class);
     }
 
     @PutMapping("updateMovieDetailsById/{id}")
     public Mono<MovieDetailsDto> updateMovieDetailsById(@PathVariable Long id, @RequestBody MovieDetailsDto movieDetailsDto){
-        String url="http://localhost:8080/api/movieDetails/updateMovieDetailsById";
-        return webClientBuilder.build().put().uri(url+"/"+id).body(BodyInserters.fromValue(movieDetailsDto)).retrieve()
+        return webClientBuilder.build().put().uri(UPDATE_MOVIE_DETAILS_BY_ID+"/"+id).body(BodyInserters.fromValue(movieDetailsDto)).retrieve()
                 .bodyToMono(MovieDetailsDto.class);
     }
 
     @DeleteMapping("deleteMovieDetailsById/{id}")
     public Mono deleteMovieDetailsById(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/deleteMovieDetailsById";
-        return webClientBuilder.build().delete().uri(url+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class);
+        return webClientBuilder.build().delete().uri(DELETE_MOVIE_DETAILS_BY_ID+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class);
     }
 
     @GetMapping("getMovieDetails1")
     public ResponseEntity<List<MovieDetailsDto>> getAllMovieDetails1(){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetails";
         MovieDetailsDto[] movieDetailsDtos=webClientBuilder.build()
-                .get().uri(url).retrieve().bodyToMono(MovieDetailsDto[].class).block();
+                .get().uri(GET_MOVIE_DETAILS).retrieve().bodyToMono(MovieDetailsDto[].class).block();
         return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(movieDetailsDtos));
     }
 
     @GetMapping("getMovieDetailsById/{id}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsById(@PathVariable Long id){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsById";
         MovieDetailsDto movieDetailsDto=webClientBuilder.build()
-                .get().uri(url+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class).block();
+                .get().uri(GET_MOVIE_DETAILS_BY_ID+"/"+id).retrieve().bodyToMono(MovieDetailsDto.class).block();
         return ResponseEntity.status(HttpStatus.OK).body(movieDetailsDto);
     }
 
     @GetMapping("getMovieDetailsByName/{name}")
     public ResponseEntity<MovieDetailsDto> getMovieDetailsByName(@PathVariable String name){
-        String url="http://localhost:8080/api/movieDetails/getMovieDetailsByName";
         MovieDetailsDto movieDetailsDto=webClientBuilder.build()
-                .get().uri(url+"/"+name).retrieve().bodyToMono(MovieDetailsDto.class).block();
+                .get().uri(GET_MOVIE_DETAILS_BY_NAME+"/"+name).retrieve().bodyToMono(MovieDetailsDto.class).block();
         return ResponseEntity.status(HttpStatus.OK).body(movieDetailsDto);
     }
 }
